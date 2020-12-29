6d45b8fe562b7899591525f7def56e48cf531016
sburns1369 <kiwishoepolish@yahoo.com>
2020-12-28 21:21:49 -0500

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1588,9 +1588,16 @@ fi
       clear
       dialog --clear \
        --title 'Where to Donate' \
-       --pause "Direct Donations to the Secure Cloud Network (SCN) Project can be made to.. BTC address: need btc wallet LTC address: do they have a LTC wallet SCN address: need main project donation address \n
-      Donations to Sburns the Script Writer can be made to...    BTC address: 32FzghE1yUZRdDmCkj3bJ6vJyXxUVPKY93  LTC address: MUdDdVr4Az1dVw47uC4srJ31Ksi5SNkC7H  SCN address: Need new address for new chain  \n
-      Donations are used to fund VPS for testing, and server cost for addnodes, and things yet implimented." \ 25 80 60
+       --pause "Direct Donations to the Secure Cloud Network (SCN) Project can be made to.. \n
+      BTC address: need btc wallet \n
+      LTC address: do they have a LTC wallet \m
+      SCN address: need main project donation address \n
+      \n
+      Donations to Sburns the Script Writer can be made to... \n
+      BTC address: 32FzghE1yUZRdDmCkj3bJ6vJyXxUVPKY93  \n
+      LTC address: MUdDdVr4Az1dVw47uC4srJ31Ksi5SNkC7H  \n
+      SCN address: Need new address for new chain  \n
+      Donations are used to fund VPS for testing, and server cost for addnodes, and things yet implimented." \ 35 90 60
   }
 Function_IP_Table_Check(){
     if [ ! -f ${DPATH}ip.tbl ]; then
