1bef5b9541bd59a0f24499bc6e4452a75d504e47
sburns1369 <kiwishoepolish@yahoo.com>
2020-12-28 19:57:34 -0500

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1585,10 +1585,11 @@ fi
   #Donation address for scriptworks and main projects
   #color code to seperate them
       clear
-      dialog --title 'Where to Donate' --msgbox 'Donations to the Secure Cloud Network (SCN) can be made to / /
-      BTC address: need btc wallet /
-      LTC address: do they have a LTC wallet /
-      SCN address: need main project donation address /' 12 40
+      dialog --title 'Where to Donate' --msgbox ' ' /
+      'Direct Donations to the Secure Cloud Network (SCN) Project can be made to' /
+      'BTC address: need btc wallet' /
+      'LTC address: do they have a LTC wallet' /
+      'SCN address: need main project donation address ' / 12 80
       echo
       echo
       echo
